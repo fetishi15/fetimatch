6faa58c6c0112f112fd66ed4f8d73c0efd0320b6
0Z0303 <momomokan30@gmail.com>
2016-09-11 08:09:00 +0000

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'users/index' 
 
   get 'users/show'
 
   devise_for :users
+  resources :users, :only => [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
